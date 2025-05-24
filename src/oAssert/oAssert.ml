8856e0d0a96966f45c01028124d36278acb45ca5
R. Kaleta <rafkaleta@onet.eu>
2025-05-24 19:13:15 +0200

    Rename explicit failure function

--- src/oAssert/oAssert.ml
+++ src/oAssert/oAssert.ml
@@ -13,10 +13,10 @@ exception Assertion_failed of string
 
 type 'a assertion = 'a Internals.assertion
 
-let assert_fail ~msg = raise @@ Assertion_failed msg
+let assertion_failed ~msg = raise @@ Assertion_failed msg
 
 let assert_that actual (Assertion f) =
   let {status; failure_message} = f actual in
   match status with
   | Passed -> ()
-  | Failed -> assert_fail ~msg:(build_message failure_message)
+  | Failed -> assertion_failed ~msg:(build_message failure_message)
---
+++ test/assertion_failed_test.ml
@@ -0,0 +1,20 @@
+(* Tests: Assertion failed. *)
+open OUnit2
+open OAssert
+
+let assertion_failed__then_failed =
+  "assertion_failed__then_failed" >:: fun _ ->
+    (* given *)
+    let message = "Custom failure message" in
+    (* when *)
+    let action () = assertion_failed ~msg:message in
+    (* then *)
+    assert_that action @@ Is.raising (Assertion_failed message)
+
+let assertion_failed_Test_list = test_list [assertion_failed__then_failed]
+
+(* assertion_failed_Test *)
+
+let assertion_failed_Test = "Tests: Assertion failed" >::: [assertion_failed_Test_list]
+
+let _ = run_test_tt_main assertion_failed_Test
